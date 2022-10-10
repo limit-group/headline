12cf95eb4ef6b5911640476d50ff6a71e5354fe4
WawinyEdwin <wawinyedwin44@gmail.com>
2022-10-10 12:27:34 +0300

    Add error 500page and improve UI

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2k19(%x!*vhpyya4$j=o!0lz)l6o0(8b5#+&4utd&ci)zox@3^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
+DEBUG = False
 
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
--- backend/urls.py
+++ backend/urls.py
@@ -29,3 +29,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 handler404 = 'writy.views.error_404'
+handler500 = 'writy.views.error_500'
